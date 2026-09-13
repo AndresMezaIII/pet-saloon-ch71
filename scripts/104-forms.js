814c5aaa2dec5fdc7eae14f235ecc9b668ebbabb
Andres M <314112705+AndresMezaIII@users.noreply.github.com>
2026-09-12 19:28:40 -0500

    post-class3

--- scripts/104-forms.js
+++ scripts/104-forms.js
@@ -1,1 +1,9 @@
-console.log(connected);
+console.log("connected");
+
+function getValues() {
+    let formProd = document.getElementById("prodInput").value;
+    let formPrice = document.getElementById("inputPrice").value;
+    let formCat = document.getElementById("catInput").value;
+    
+    alert(`${formProd} ${formCat} $${formPrice}`);
+}

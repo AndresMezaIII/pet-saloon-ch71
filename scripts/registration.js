2427769f713c1376f5c90be3cbb6c44d6efa43be
Andres Meza III <314112705+AndresMezaIII@users.noreply.github.com>
2026-09-15 19:16:11 -0500

    thinning out code

--- scripts/registration.js
+++ scripts/registration.js
@@ -4,7 +4,7 @@ let registeredPets = [
     Age: 3,
     Gender: "male",
     Service: "nails",
-    Breed: "Giant Schnauzer"
+    Breed: "Giant Schnauzer",
   },
 
   {
@@ -12,7 +12,7 @@ let registeredPets = [
     Age: 4,
     Gender: "female",
     Service: "full",
-    Breed: "Norwegian Lundehund"
+    Breed: "Norwegian Lundehund",
   },
 
   {
@@ -20,8 +20,8 @@ let registeredPets = [
     Age: 7,
     Gender: "female",
     Service: "brushing",
-    Breed: "Maine Coon"
-  }
+    Breed: "Maine Coon",
+  },
 ];
 
 //used summaries from Google to clarify using .createElement and .appendChild
@@ -38,30 +38,6 @@ oPetList.appendChild(oLiOne);
 oPetList.appendChild(oLiTwo);
 oPetList.appendChild(oLiThree);
 
-oLiOne.textContent = 
-  `${registeredPets[0].Name} ${registeredPets[0].Age} year old ${registeredPets[0].Breed}`;
-oLiTwo.textContent = 
-  `${registeredPets[1].Name} ${registeredPets[1].Age} year old ${registeredPets[1].Breed} `;
-oLiThree.textContent = 
-  `${registeredPets[2].Name} ${registeredPets[2].Age} year old ${registeredPets[2].Breed}`;
-
-
-function addPet () {
-  let petName = document.getElementById("name").value;
-  let petAge = document.getElementById("age").value;
-  let petBreed = document.getElementById("breed").value;
-  let petService = document.getElementById("services").value;
-  let petGender = document.getElementById("gender").value;
-  let petDemo = [];
-  
-  petDemo.push({Name: petName, Age: Number(petAge), Gender: petGender, Service: petService, Breed: petBreed});
-
-  //learned union of arrays through Google
-  registeredPets = [...registeredPets, ...petDemo];
-  
-  const getList = document.getElementById("regPets");
-  const liItem = document.createElement("li");
-  
-  oPetList.appendChild(liItem);
-  liItem.textContent = `${petName} ${petAge} year old ${petBreed}`;
-};
+oLiOne.textContent = `${registeredPets[0].Name} ${registeredPets[0].Age} year old ${registeredPets[0].Breed}`;
+oLiTwo.textContent = `${registeredPets[1].Name} ${registeredPets[1].Age} year old ${registeredPets[1].Breed} `;
+oLiThree.textContent = `${registeredPets[2].Name} ${registeredPets[2].Age} year old ${registeredPets[2].Breed}`;

9ea1f8a190fef83a233093be81b82d77810fad6c
Andres M <314112705+AndresMezaIII@users.noreply.github.com>
2026-09-20 14:52:57 -0600

    assignment 3 66%

--- scripts/104-students-form.js
+++ scripts/104-students-form.js
@@ -1,53 +1,51 @@
 // ==== OBJECT CONSTRUCTOR ===
-function Student(name, lastname, cohort, email, classValue){
-    this.name = name;
-    this.lastname = lastname;
-    this.cohort = cohort;
-    this.email = email;
-    this.class = classValue;
+function Student(name, lastname, cohort, email, classValue) {
+  this.name = name;
+  this.lastname = lastname;
+  this.cohort = cohort;
+  this.email = email;
+  this.class = classValue;
 }
 
 // ==== REGISTER STUDENT ===
-function registerStudent(event){
-    event.preventDefault();
+function registerStudent(event) {
+  event.preventDefault();
 
-    let name = document.getElementById("studentName").value;
-    let lastname = document.getElementById("studentLastname").value;
-    let cohort = document.getElementById("studentCohort").value;
-    let email = document.getElementById("studentEmail").value;
-    let classValue = document.getElementById("studentClass").value;  
+  let name = document.getElementById("studentName").value;
+  let lastname = document.getElementById("studentLastname").value;
+  let cohort = document.getElementById("studentCohort").value;
+  let email = document.getElementById("studentEmail").value;
+  let classValue = document.getElementById("studentClass").value;
 
-    let newStudent = new Student(name, lastname, cohort, email, classValue);
-    
-    let body = document.getElementById("body");
-    const row = document.createElement("tr");
+  let newStudent = new Student(name, lastname, cohort, email, classValue);
 
-    row.innerHTML += `
+  let body = document.getElementById("body");
+  const row = document.createElement("tr");
+
+  row.innerHTML += `
             <td> ${newStudent.name} </td>
             <td> ${newStudent.lastname} </td>
             <td> ${newStudent.cohort} </td>
             <td> ${newStudent.email} </td>
             <td> ${newStudent.class} </td>
-            <td> <button class="btn btn-danger delete-btn">Delete</button> </td>
+            <td> <button class="btn btn-danger del-btn">Delete</button></td>
     `;
 
-    // Detele Functionality
-    row.querySelector(".delete-btn").addEventListener("click", function(){
+  // Detele Functionality
+  row.querySelector(".del-btn").addEventListener("click", function () {
+    let confirmation = confirm("Are you sure you want to delete this studen?");
 
-        let confirmation = confirm("Are you sure you want to delete this studen?");
+    if (confirmation) {
+      row.remove();
+    }
+  });
 
-        if(confirmation){
-            row.remove();
-        }
-    });
-
-    body.appendChild(row);
+  body.appendChild(row);
 }
-
 
 // ==== ANONYMOUS FUNCTION - TEST ===
 //const button = document.querySelector("#testButton");
 
-document.querySelector("#testButton").addEventListener("click", function(){
-    alert("Button Clicked!");
+document.querySelector("#testButton").addEventListener("click", function () {
+  alert("Button Clicked!");
 });
--- scripts/registration.js
+++ scripts/registration.js
@@ -25,23 +25,28 @@ let registeredPets = [
 ];
 
 //used summaries from Google to clarify using .createElement and .appendChild
-const oList = document.getElementById("regPets");
-const oPetList = document.createElement("ol");
-const oLiOne = document.createElement("li");
-const oLiTwo = document.createElement("li");
-const oLiThree = document.createElement("li");
-
+const OLIST = document.getElementById("regPets");
+const OPETLIST = document.createElement("ol");
+const OLIONE = document.createElement("li");
+const OLITWO = document.createElement("li");
+const OLITHREE = document.createElement("li");
+const OLIFOUR = document.createElement("li");
+const OLIFIVE = document.createElement("li");
+const OLISIX = document.createElement("li");
 //I don't understand why using this line and the next is necessary vs. one over the other, but the code doesn't work without them both.
-document.body.appendChild(oPetList);
-oList.appendChild(oPetList);
+document.body.appendChild(OPETLIST);
+OLIST.appendChild(OPETLIST);
 
-oPetList.appendChild(oLiOne);
-oPetList.appendChild(oLiTwo);
-oPetList.appendChild(oLiThree);
+OPETLIST.appendChild(OLIONE);
+OPETLIST.appendChild(OLITWO);
+OPETLIST.appendChild(OLITHREE);
+OPETLIST.appendChild(OLIFOUR);
+OPETLIST.appendChild(OLIFIVE);
+OPETLIST.appendChild(OLISIX);
 
-oLiOne.textContent = `${registeredPets[0].Name}`;
-oLiTwo.textContent = `${registeredPets[1].Name}`;
-oLiThree.textContent = `${registeredPets[2].Name}`;
+OLIONE.textContent = `${registeredPets[0].Name}`;
+OLITWO.textContent = `${registeredPets[1].Name}`;
+OLITHREE.textContent = `${registeredPets[2].Name}`;
 
 function PetInfo(Name, Age, Breed, Gender, Service) {
   this.Name = Name;
@@ -51,39 +56,86 @@ function PetInfo(Name, Age, Breed, Gender, Service) {
   this.Service = Service;
 }
 
-let PetOne = new PetInfo(
+const PETONE = new PetInfo(
   "Bullseye",
   3,
   "American Pit Bull",
   "male",
   "Ear Cleaning",
 );
-let PetTwo = new PetInfo("Stretch", 5, "Serval", "female", "Teeth Cleaning");
-let PetThree = new PetInfo("Dodge", 2, "Border Collie", "male", "Full Service");
+const PETTWO = new PetInfo("Stretch", 5, "Serval", "female", "Teeth Cleaning");
+const PETTHREE = new PetInfo(
+  "Dodge",
+  2,
+  "Border Collie",
+  "male",
+  "Full Service",
+);
 
-registeredPets.push(PetOne, PetTwo, PetThree);
+OLIFOUR.textContent = `${PETONE.Name}`;
+OLIFIVE.textContent = `${PETTWO.Name}`;
+OLISIX.textContent = `${PETTHREE.Name}`;
 
-const getTable = document.getElementById("tbody");
-getTable.textContent = "";
+registeredPets.push(PETONE, PETTWO, PETTHREE);
+
+const GETTABLE = document.getElementById("tbody");
+GETTABLE.textContent = "";
 
 function importDataToRows() {
   for (let i = 0; i < registeredPets.length; i++) {
-    const tableRow = document.createElement("tr");
-    const pet = registeredPets[i];
+    const TABLEROW = document.createElement("tr");
+    const PET = registeredPets[i];
 
     for (const value of [
-      pet.Name,
-      pet.Age,
-      pet.Breed,
-      pet.Gender,
-      pet.Service,
+      PET.Name,
+      PET.Age,
+      PET.Breed,
+      PET.Gender,
+      PET.Service,
     ]) {
-      const data = document.createElement("td");
-      data.textContent = value;
-      tableRow.appendChild(data);
+      const DATA = document.createElement("td");
+      DATA.textContent = value;
+      TABLEROW.appendChild(DATA);
     }
-    getTable.appendChild(tableRow);
+    GETTABLE.appendChild(TABLEROW);
   }
 }
 
 importDataToRows();
+
+function displayRow(event) {
+  event.preventDefault();
+
+  let name = document.getElementById("tdOne").value;
+  let age = document.getElementById("tdTwo").value;
+  let breed = document.getElementById("tdThree").value;
+  let gender = document.getElementById("tdFour").value;
+  let service = document.getElementById("tdFive").value;
+
+  let newPet = new PetInfo(name, age, breed, gender, service);
+
+  let tbody = document.getElementById("tbody");
+  const ROW = document.createElement("tr");
+
+  ROW.innerHTML += `
+            <td> ${newPet.Name} </td>
+            <td> ${newPet.Age} </td>
+            <td> ${newPet.Breed} </td>
+            <td> ${newPet.Gender} </td>
+            <td> ${newPet.Service} </td>
+            <td> <button class="btn btn-danger del-btn">Delete</button></td>
+    `;
+
+  // Detele Functionality
+  ROW.querySelector(".del-btn").addEventListener("click", function () {
+    let confirmation = confirm(
+      "Are you sure you want to delete this reservation?",
+    );
+
+    if (confirmation) {
+      ROW.remove();
+    }
+  });
+
+  tbody.appendChild(ROW);
+}

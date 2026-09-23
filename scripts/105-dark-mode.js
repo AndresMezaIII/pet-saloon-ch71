3fd98526b59580cac288280fdc005f69cd34393b
Andres M <314112705+AndresMezaIII@users.noreply.github.com>
2026-09-22 21:14:32 -0600

    end-class 2

--- scripts/105-dark-mode.js
+++ scripts/105-dark-mode.js
@@ -1,3 +1,12 @@
 $("#change-mode").click(function () {
   $("body").toggleClass("dark-mode");
+  //Is the class applied to the body?
+  const ISDARK = $("body").hasClass("dark-mode");
+
+  /*if (ISDARK) {
+    $("h1").text("🌓 Dark Mode");
+  } else {
+    $("h1").text("☀️ Light Mode");
+  }*/
+  ISDARK ? $("h1").text("🌓 Dark Mode") : $("h1").text("☀️ Light Mode");
 });
